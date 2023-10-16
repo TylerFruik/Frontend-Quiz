7879d7f133a78be4100ea4cc855fa4c48156cc2f
TylerFruik <tylerfruik@gmail.com>
2023-10-16 02:39:17 -0500

    JS quite well underway. Questions randomize and will be popped each time a question is passed. Timer is not yet implemented nor are the answers displayed/coded to be right or wrong. High scores will also need to be done. Great work tonight regardless.

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,84 @@
 // #region ELEMENT SELECTORS
-const timer = document.querySelector("time-counter");
-const answerButtons = document.querySelectorAll('.answer-btn');
-//#endregion
-let qOrder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+const timer = document.querySelector('.time-counter');
+const answerBtns = document.querySelectorAll('.answer-btn');
+const startQuizBtn = document.getElementById('start-quiz');
+const game = document.querySelector('.game');
+const mainMenu = document.querySelector('.main-menu');
+const quitBtn = document.querySelector('.quit-btn');
+const question = document.querySelector('.question');
 
+//#endregion
 
 // #region QUESTIONS
-const q1 = "Question 1";
-const q2 = "Question 2";
-const q3 = "Question 3";
-const q4 = "Question 4";
-const q5 = "Question 5";
-const q6 = "Question 6";
-const q7 = "Question 7";
-const q8 = "Question 8";
-const q9 = "Question 9";
-const q10 = "Question 10";
-const q11 = "Question 11";
-const q12 = "Question 12";
-const q13 = "Question 13";
-const q14 = "Question 14";
-const q15 = "Question 15";
-const q16 = "Question 16";
-const q17 = "Question 17";
-const q18 = "Question 18";
-const q19 = "Question 19";
-const q20 = "Question 20";
+const questions = [
+    'Question 1',
+    'Question 2',
+    'Question 3',
+    'Question 4',
+    'Question 5',
+    'Question 6',
+    'Question 7',
+    'Question 8',
+    'Question 9',
+    'Question 10',
+    'Question 11',
+    'Question 12',
+    'Question 13',
+    'Question 14',
+    'Question 15',
+    'Question 16',
+    'Question 17',
+    'Question 18',
+    'Question 19',
+    'Question 20'
+];
 //#endregion
 
 // #region FUNCTIONS
+function gameStart() {
+    for (let i = questions.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [questions[i], questions[j]] = [questions[j], questions[i]];
+    }
+    question.value = getQ();
+}
+
+function getQ() {
+    if (questions.length > 0) {
+        question.textContent = questions.pop();
+    } else {
+        gameEnd();
+    }
+}
+
 function checkAnswer(submission) {
     console.log(submission);
+}
+
+function gameEnd() {
+    console.log('You did it!');
 }
 // #endregion
 
 
 // #region LISTENERS
-answerButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        button.classList.toggle('selected');
-        checkAnswer(button.id);
+answerBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('selected');
+        checkAnswer(btn.id);
     })
+})
+
+startQuizBtn.addEventListener('click', () => {
+    game.style.display = 'grid';
+    mainMenu.style.display = 'none';
+    gameStart()
+})
+
+quitBtn.addEventListener('click', () => {
+    game.style.display = 'none';
+    mainMenu.style.display = 'flex';
 })
 // #endregion 
 
+console.log(questions);
